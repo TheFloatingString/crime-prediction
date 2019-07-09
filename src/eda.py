04e4e02921beb642556dbcd4d8dc988680833450
TheMatrixMaster <stephen.lu.2002@gmail.com>
2019-07-09 09:27:42 -0400

    started model training

--- src/eda.py
+++ src/eda.py
@@ -1,13 +1,13 @@
 import pandas as pd 
 import numpy as np 
 
-weather_data_mtl = pd.read_csv("datasets/climate-daily.csv")
+weather_data_mtl = pd.read_csv("../datasets/climate-daily.csv")
 
-crime_data = pd.read_csv("datasets/pdq_3.csv", index_col='Unnamed: 0').drop(columns=['jour','nuit','soir'])
+crime_data = pd.read_csv("../datasets/pdq_3.csv", index_col='Unnamed: 0').drop(columns=['jour','nuit','soir'])
 print("crime data shape")
 print(crime_data.shape)
 
-preprocessed_weather_data_mtl = np.load("preprocessed_data/X_train_weather.npy")
+preprocessed_weather_data_mtl = np.load("../preprocessed_data/X_train_weather.npy")
 preprocessed_weather_data_mtl[preprocessed_weather_data_mtl == np.inf] = 0
 
 print("weather data shape")
