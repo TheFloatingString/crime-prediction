875da34573cdb775ed18cae5fd8c1df4c8adc71b
Laurence Liang <laurencel2001@gmail.com>
2019-07-10 15:57:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,17 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 X_min_max_scaler = joblib.load("static/saved_models/X_min_max_scaler.pkl")
 
+temp, wind, humidity = get_climate_data("https://api.darksky.net/forecast/2dfdcc237ebdb8ba4c7b2360152a9a87/45.508889,-73.561667")
+nasdaq = si.get_live_price('^ixic')
+default_X_input_data = [ nasdaq, nasdaq ,nasdaq,nasdaq
+,1.93998000e+09,2.49303893e-02,8.78226158e-03,2.52015299e-02
+,7.71050944e-03,-4.06729205e-02, temp, temp
+,temp,0.00000000e+00,0.00000000e+00,1.00000000e+00
+,wind,3.90000000e+00,humidity,humidity
+,5.22388060e-02,3.52941176e-01,-4.52261307e-02,0.00000000e+00
+,0.00000000e+00,-9.62962963e-01,-4.12698413e-01,-1.52173913e-01
+,0.00000000e+00,0.00000000e+00]
+default_X_input_data = X_min_max_scaler.fit_transform(np.array(default_X_input_data).reshape(1,-1))[0]
 
 # home page
 @app.route('/', methods=['GET', 'POST'])
@@ -52,17 +63,7 @@ def home():
 
 @app.route('/get_pdq_data/<pdq_number>', methods=['GET'])
 def get_pdq_data(pdq_number):
-	temp, wind, humidity = get_climate_data("https://api.darksky.net/forecast/2dfdcc237ebdb8ba4c7b2360152a9a87/45.508889,-73.561667")
-	nasdaq = si.get_live_price('^ixic')
-	default_X_input_data = [ nasdaq, nasdaq ,nasdaq,nasdaq
-	,1.93998000e+09,2.49303893e-02,8.78226158e-03,2.52015299e-02
-	,7.71050944e-03,-4.06729205e-02, temp, temp
-	,temp,0.00000000e+00,0.00000000e+00,1.00000000e+00
-	,wind,3.90000000e+00,humidity,humidity
-	,5.22388060e-02,3.52941176e-01,-4.52261307e-02,0.00000000e+00
-	,0.00000000e+00,-9.62962963e-01,-4.12698413e-01,-1.52173913e-01
-	,0.00000000e+00,0.00000000e+00]
-	default_X_input_data = X_min_max_scaler.fit_transform(np.array(default_X_input_data).reshape(1,-1))[0]
+
 
 	return compute_level(default_X_input_data, str(pdq_number))
 
