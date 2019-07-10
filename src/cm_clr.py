5a0a78a4e2b9a024d210e41be3b341620b39547c
Om Agarwal <om000developer@gmail.com>
2019-07-10 12:52:45 -0300

    Data analysis of random forest

---
+++ src/cm_clr.py
@@ -0,0 +1,124 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_confusion_matrix(cm, classes, title, normalize=True, cmap=plt.cm.BuPu):
+    
+    from itertools import product
+    
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    
+    plt.rcParams["figure.figsize"] = (7,7)
+    plt.imshow(cm, cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=25)
+    plt.yticks(tick_marks, classes, rotation=25)
+    
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    
+    return i,j
+
+#-------------------------------------------------------------------
+    
+import matplotlib.pyplot as plt
+import numpy as np
+
+def show_values(pc, fmt="%.2f", **kw):
+    
+    pc.update_scalarmappable()
+    ax = pc.axes
+    for p, color, value in zip(pc.get_paths(), pc.get_facecolors(), pc.get_array()):
+        x, y = p.vertices[:-2, :].mean(0)
+        if np.all(color[:3] > 0.5):
+            color = (0.0, 0.0, 0.0)
+        else:
+            color = (1.0, 1.0, 1.0)
+        ax.text(x, y, fmt % value, ha="center", va="center", color=color, **kw)
+
+def cm2inch(*tupl):
+    
+    inch = 2.54
+    if type(tupl[0]) == tuple:
+        return tuple(i/inch for i in tupl[0])
+    else:
+        return tuple(i/inch for i in tupl)
+
+def heatmap(AUC, title, xlabel, ylabel, xticklabels, yticklabels, figure_width=40, figure_height=20, correct_orientation=False, cmap='RdBu'):
+    
+    fig, ax = plt.subplots()    
+    #c = ax.pcolor(AUC, edgecolors='k', linestyle= 'dashed', linewidths=0.2, cmap='RdBu', vmin=0.0, vmax=1.0)
+    c = ax.pcolor(AUC, edgecolors='k', linestyle= 'dashed', linewidths=0.2, cmap=cmap)
+
+    ax.set_yticks(np.arange(AUC.shape[0]) + 0.5, minor=False)
+    ax.set_xticks(np.arange(AUC.shape[1]) + 0.5, minor=False)
+
+    #ax.set_xticklabels(np.arange(1,AUC.shape[1]+1), minor=False)
+    ax.set_xticklabels(xticklabels, minor=False)
+    ax.set_yticklabels(yticklabels, minor=False)
+
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)      
+    plt.xlim( (0, AUC.shape[1]) )
+
+    ax = plt.gca()    
+    for t in ax.xaxis.get_major_ticks():
+        t.tick1On = False
+        t.tick2On = False
+    for t in ax.yaxis.get_major_ticks():
+        t.tick1On = False
+        t.tick2On = False
+
+    plt.colorbar(c)
+    show_values(c)
+
+    if correct_orientation:
+        ax.invert_yaxis()
+        ax.xaxis.tick_top()       
+
+    fig = plt.gcf()
+    #fig.set_size_inches(cm2inch(40, 20))
+    #fig.set_size_inches(cm2inch(40*4, 20*4))
+    fig.set_size_inches(cm2inch(figure_width, figure_height))
+
+def plot_classification_report(classification_report, title, cmap='RdBu'):
+    
+    lines = classification_report.split('\n')
+
+    classes = []
+    plotMat = []
+    support = []
+    class_names = []
+    for line in lines[2 : (len(lines) - 2)]:
+        t = line.strip().split()
+        if len(t) < 2: continue
+        classes.append(t[0])
+        v = [float(x) for x in t[1: len(t) - 1]]
+        support.append(int(t[-1]))
+        class_names.append(t[0])
+        #print(v)
+        plotMat.append(v)
+
+    #print('plotMat: {0}'.format(plotMat))
+    #print('support: {0}'.format(support))
+
+    xlabel = 'Metrics'
+    ylabel = 'Classes'
+    xticklabels = ['Precision', 'Recall', 'F1-score']
+    yticklabels = ['{0} ({1})'.format(class_names[idx], sup) for idx, sup  in enumerate(support)]
+    figure_width = 25
+    figure_height = len(class_names) + 7
+    correct_orientation = False
+    heatmap(np.array(plotMat), title, xlabel, ylabel, xticklabels, yticklabels, figure_width, figure_height, correct_orientation, cmap=cmap)
--- src/v2_generate_random_forest.py
+++ src/v2_generate_random_forest.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import csv
 import numpy as np
 import random
 
@@ -9,51 +8,129 @@ from sklearn.externals import joblib
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
-from math import *
+from sklearn import metrics as sm
+from cm_clr import plot_classification_report, plot_confusion_matrix
+from scipy import interp
+from itertools import cycle
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (6,6)
+
+from math import sqrt
 
 
 pdq_list = [1.0, 3.0, 4.0, 5.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 15.0, 16.0, 20.0, 21.0, 22.0, 23.0, 24.0, 26.0, 27.0, 30.0, 31.0, 33.0, 35.0, 38.0, 39.0, 42.0, 44.0, 45.0, 46.0, 48.0, 49.0, 50.0, 55.0]
 
 for pdq_number in pdq_list:
-
-	print("pdq_number: "+str(pdq_number))
-	df_unique_crime_pdq = pd.read_csv("datasets/pdq_"+str(int(pdq_number))+".csv", index_col="Unnamed: 0").drop(columns=["jour", "nuit", "soir"])
-	print(df_unique_crime_pdq.head())
-	# df_nasdaq = pd.read_csv("datasets/NASDAQ_DATA.csv", index_col="Date")
-	# print(df_nasdaq.head())
-	nasdaq_data = np.load("preprocessed_data/X_train_nasdaq.npy")
-	# print(nasdaq_data[0])
-	weather_array = np.load("preprocessed_data/X_train_weather.npy")
-
-	# x[~np.isnan(x).any(axis=1)]
-	# x[x == -inf] = 0
-
-	X_data = np.concatenate((nasdaq_data, weather_array), axis=1)
-	y_data = df_unique_crime_pdq.values
-
-	X_data[X_data == np.inf] = 0
-	X_data[X_data == -np.inf] = 0
-
-	# scaling data
-	X_min_max_scaler = MinMaxScaler()
-	X_data = X_min_max_scaler.fit_transform(X_data)
-	joblib.dump(X_min_max_scaler, "static/saved_models/X_min_max_scaler.pkl")
-
-	X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2)
-
-	# print(weather_array[0])
-	clf = RandomForestRegressor()
-	clf.fit(X_data, y_data)
-	joblib.dump(clf, "static/saved_models/v2_rn_forest_"+str(pdq_number)+".pkl")
-	print(clf.score(X_test, y_test))
-
-	# mse
-	predictions = clf.predict(X_test)
-	mse = sqrt(mean_squared_error(predictions, y_test))
-	print("mse: "+str(mse))
-
-	random_test_index = random.randrange(0, len(X_test))
-	# print(random_test_index)
-
-	# print(clf.predict([X_test[random_test_index]]))
-	# print(y_test[random_test_index])
+    
+    print("pdq_number: "+str(pdq_number))
+    df_unique_crime_pdq = pd.read_csv("../datasets/pdq_"+str(int(pdq_number))+".csv", index_col="Unnamed: 0").drop(columns=["jour", "nuit", "soir"])
+    print(df_unique_crime_pdq.head())
+    # df_nasdaq = pd.read_csv("datasets/NASDAQ_DATA.csv", index_col="Date")
+    # print(df_nasdaq.head())
+    nasdaq_data = np.load("../preprocessed_data/X_train_nasdaq.npy")
+    # print(nasdaq_data[0])
+    weather_array = np.load("../preprocessed_data/X_train_weather.npy")
+    
+    # x[~np.isnan(x).any(axis=1)]
+    # x[x == -inf] = 0
+    
+    X_data = np.concatenate((nasdaq_data, weather_array), axis=1)
+    y_data = df_unique_crime_pdq.values
+    
+    X_data[X_data == np.inf] = 0
+    X_data[X_data == -np.inf] = 0
+    
+    # scaling data
+    X_min_max_scaler = MinMaxScaler()
+    X_data = X_min_max_scaler.fit_transform(X_data)
+    joblib.dump(X_min_max_scaler, "../static/saved_models/X_min_max_scaler.pkl")
+    
+    X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2)
+    
+    # print(weather_array[0])
+    clf = RandomForestRegressor()
+    clf.fit(X_train, y_train)
+    joblib.dump(clf, "../static/saved_models/v2_rn_forest_"+str(pdq_number)+".pkl")
+    print(clf.score(X_test, y_test))
+    
+    # mse
+    y_pred = clf.predict(X_test)
+    mse = sqrt(mean_squared_error(y_pred, y_test))
+    print("rmse: "+str(mse))
+    
+    random_test_index = random.randrange(0, len(X_test))
+    # print(random_test_index)
+    
+    # print(clf.predict([X_test[random_test_index]]))
+    # print(y_test[random_test_index])
+    
+    predictions, actuals = [], []
+    for i in range(len(y_pred)): 
+        predictions.append(np.where(y_pred[i] == np.max(y_pred[i]))[0][0])
+        actuals.append(np.where(y_test[i] == np.max(y_test[i]))[0][0])
+    
+    categories = ["No_Crime", "Introduction", "Vol_dans_/_sur_véhicule_à_moteur", "Vol_de_véhicule_à_moteur", "Méfait", "Vols_qualifiés", "Infractions_entrainant_la_mort"]
+    n_classes = len(categories)
+    
+    cm = sm.confusion_matrix(predictions, actuals, labels=[i for i in range(n_classes)])
+    plot_confusion_matrix(cm, classes=categories, title='Confusion Matrix')
+    plt.savefig("../static/saved_models/v2_rn_forest_"+str(pdq_number)+"_cnf_mtx.png", dpi=200, format='png', bbox_inches='tight'); plt.close();
+    
+    for i in range(len(categories)): 
+        categories[i] = categories[i].replace("&", "and")
+        categories[i] = categories[i].replace(" ", "_")
+    
+    cr = sm.classification_report(actuals, predictions, labels=[i for i in range(n_classes)], target_names=categories); cr = cr.split("\n")
+    plot_classification_report(cr[0] + '\n\n' + cr[2] + '\n' + cr[3] + '\n' + cr[4] + '\n' + cr[5] + '\n' + cr[6] + '\n' + cr[7] + '\n' + cr[8] + '\n' + cr[10] + '\n', title = 'Classification Report')
+    plt.savefig("../static/saved_models/v2_rn_forest_"+str(pdq_number)+"_cls_rep.png", dpi=200, format='png', bbox_inches='tight'); plt.close();
+    
+    pd.Series([ y[0] for y in y_pred ]).plot.hist(grid=True, bins=20, rwidth=0.9, color='#607c8e')
+    plt.xlabel('Values'); plt.ylabel('Frequency')
+    plt.title('Predictions (y) Ranges Histogram')
+    plt.grid(axis='y', alpha=0.75)
+    plt.savefig("../static/saved_models/v2_rn_forest_"+str(pdq_number)+"_pred_dist.png", dpi=200, format='png', bbox_inches='tight'); plt.close();
+    
+    lw = 2
+    
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = sm.roc_curve(y_test[:, i], y_pred[:, i])
+        roc_auc[i] = sm.auc(fpr[i], tpr[i])
+    
+    fpr["micro"], tpr["micro"], _ = sm.roc_curve(y_test.ravel(), y_pred.ravel())
+    roc_auc["micro"] = sm.auc(fpr["micro"], tpr["micro"])
+    
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+    
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes): mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+    
+    mean_tpr /= n_classes
+    
+    fpr["macro"] = all_fpr; tpr["macro"] = mean_tpr
+    roc_auc["macro"] = sm.auc(fpr["macro"], tpr["macro"])
+    
+    plt.figure()
+    plt.plot(fpr["micro"], tpr["micro"],
+             label='micro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["micro"]), color='deeppink', linestyle=':', linewidth=4)
+    
+    plt.plot(fpr["macro"], tpr["macro"],
+             label='macro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["macro"]), color='navy', linestyle=':', linewidth=4)
+    
+    colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+    for i, color in zip(range(n_classes), colors):
+        plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+                 label='ROC curve of class {0} (area = {1:0.2f})'
+                 ''.format(i, roc_auc[i]))
+    
+    plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+    plt.xlim([0.0, 1.0]); plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate'); plt.ylabel('True Positive Rate')
+    plt.title('Some extension of Receiver operating characteristic to multi-class')
+    plt.legend(loc="lower right")
+    plt.savefig("../static/saved_models/v2_rn_forest_"+str(pdq_number)+"_roc_auc.png", dpi=200, format='png', bbox_inches='tight'); plt.close();
