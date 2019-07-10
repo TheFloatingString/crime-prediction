8a5aaeabd15b4184d11c505d49ae6a427d68aebe
Laurence Liang <laurencel2001@gmail.com>
2019-07-10 15:41:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,14 @@ X_min_max_scaler = joblib.load("static/saved_models/X_min_max_scaler.pkl")
 def home():
 	temp, wind, humidity = get_climate_data("https://api.darksky.net/forecast/2dfdcc237ebdb8ba4c7b2360152a9a87/45.508889,-73.561667")
 	nasdaq = si.get_live_price('^ixic')
+	default_X_input_data = [ nasdaq, nasdaq ,nasdaq,nasdaq
+	,1.93998000e+09,2.49303893e-02,8.78226158e-03,2.52015299e-02
+	,7.71050944e-03,-4.06729205e-02, temp, temp
+	,temp,0.00000000e+00,0.00000000e+00,1.00000000e+00
+	,wind,3.90000000e+00,humidity,humidity
+	,5.22388060e-02,3.52941176e-01,-4.52261307e-02,0.00000000e+00
+	,0.00000000e+00,-9.62962963e-01,-4.12698413e-01,-1.52173913e-01
+	,0.00000000e+00,0.00000000e+00]
 	return render_template('index.html', draw_pdq_code=return_coords(default_X_input_data), temp=str(temp)[:4], wind=wind, humidity=humidity, nasdaq=str(nasdaq)[:6])
 
 @app.route('/get_pdq_data/<pdq_number>', methods=['GET'])
